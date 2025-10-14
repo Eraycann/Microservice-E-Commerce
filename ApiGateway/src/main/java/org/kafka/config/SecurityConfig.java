3d4a04f8ae4ed80e6b52761653e5502abf15f792
Nisa Gökdemir <113767004+NisaGokdemir@users.noreply.github.com>
2025-10-14 03:15:44 +0300

    API Gateway'e OAuth2 (Resource Server & Client) güvenlik yapılandırması eklendi ve Gradle bağımlılıkları tanımlandı

---
+++ ApiGateway/src/main/java/org/kafka/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.kafka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        // Auth ve Google redirect yolları serbest
+                        .pathMatchers("/api/auth/**", "/login/oauth2/**").permitAll()
+                        .anyExchange().authenticated() // Diğer tüm yollar JWT doğrulama gerektirir
+                )
+                // OAuth2 JWT doğrulamasını etkinleştirir
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
