bc5e3e3b40a3ec2a5601a5bbe0fc185c5f57b0cb
Nisa Gökdemir <nisagokdemir@gmail.com>
2025-11-30 01:32:10 +0300

    order domain için temel ayarlar(main sınıf, bağımlılıklar, yml dosyası) yapıldı.

---
+++ OrderService/src/main/java/org/kafka/OrderServiceApp.java
@@ -0,0 +1,11 @@
+package org.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderServiceApp {
+    public static void main(String[] args) {SpringApplication.run(OrderServiceApp.class, args);}
+}
