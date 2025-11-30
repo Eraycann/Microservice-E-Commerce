c844bba05a07fff7305f5d6707a65ae41586b683
Nisa Gökdemir <nisagokdemir@gmail.com>
2025-11-30 03:05:18 +0300

    product domain içerisine cart erişimi için yeni istek ayarlandı. ilgili ürünün sepet için detayları getiriliyor.

--- ProductService/src/main/java/org/kafka/mapper/ProductMapper.java
+++ ProductService/src/main/java/org/kafka/mapper/ProductMapper.java
@@ -1,9 +1,6 @@
 package org.kafka.mapper;
 
-import org.kafka.dto.ProductCreateRequestDto;
-import org.kafka.dto.ProductDetailResponseDto;
-import org.kafka.dto.ProductImageResponseDto;
-import org.kafka.dto.ProductUpdateRequestDto;
+import org.kafka.dto.*;
 import org.kafka.model.Product;
 import org.kafka.model.ProductImage;
 import org.mapstruct.Mapper;
@@ -15,7 +12,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-    // --- CREATE ---
+
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "slug", ignore = true)
     @Mapping(target = "category", ignore = true)
@@ -26,23 +23,17 @@ public interface ProductMapper {
     @Mapping(target = "createdAt", ignore = true)
     Product toEntity(ProductCreateRequestDto dto);
 
-    // --- READ (RESPONSE) ---
+
     @Mapping(source = "category.name", target = "categoryName")
     @Mapping(source = "brand.name", target = "brandName")
     @Mapping(source = "inventory.stockCount", target = "stockCount")
     @Mapping(source = "specs.specsData", target = "specsData")
-    // MapStruct, "images" alanını otomatik eşleştirir ancak alt metodun varlığını görmek ister.
     ProductDetailResponseDto toDetailResponse(Product product);
 
     List<ProductDetailResponseDto> toDetailResponseList(List<Product> products);
 
-    // --- YENİ EKLENEN METOT ---
-    // Tek bir ProductImage -> ProductImageResponseDto dönüşümü.
-    // MapStruct bunu kullanarak List dönüşümünü otomatik halleder.
     ProductImageResponseDto toImageDto(ProductImage productImage);
 
-
-    // --- UPDATE ---
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "slug", ignore = true)
     @Mapping(target = "category", ignore = true)
@@ -52,4 +43,9 @@ public interface ProductMapper {
     @Mapping(target = "images", ignore = true) // Resim güncellemesi ayrı yönetilir
     @Mapping(target = "createdAt", ignore = true)
     void updateEntity(@MappingTarget Product product, ProductUpdateRequestDto dto);
+
+
+    @Mapping(source = "inventory.stockCount", target = "stockCount")
+    @Mapping(target = "mainImageUrl", ignore = true) // Bu alan, özel mantık gerektirir
+    ProductCartDetailDto toCartDetailDto(Product product);
 }
