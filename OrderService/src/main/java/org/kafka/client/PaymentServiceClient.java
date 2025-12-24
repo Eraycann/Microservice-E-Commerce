d1aa1ad091a50630dc631445f6130c1efd92b480
Eraycan Sivri <eraycansivri@hotmail.com>
2025-12-24 19:34:39 +0300

    feat(order-service): eureka kullanarak ip'ye ulaşabildiği için url kaldırdım.

--- OrderService/src/main/java/org/kafka/client/PaymentServiceClient.java
+++ OrderService/src/main/java/org/kafka/client/PaymentServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", url = "http://localhost:8090")
+@FeignClient(name = "payment-service")
 public interface PaymentServiceClient {
 
     @PostMapping("/api/payments/process")
